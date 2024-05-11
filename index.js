bee9a362d46d8d0ca265d8a61e3e27a026ce7144
refjikdolkre <msjcrs.engineer@hotmail.com>
2024-05-11 12:39:11 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function combinationSum(candidates, target) {
-  const result = [];
-  backtrack(0, [], 0);
-  return result;
-  function backtrack(start, current, sum) {
-    if (sum === target) {
-      result.push([...current]);
-      return;
-    }
-    if (sum > target || start === candidates.length) return;
-    for (let i = start; i < candidates.length; i++) {
-      current.push(candidates[i]);
-      backtrack(i, current, sum + candidates[i]);
-      current.pop();
-    }
-  }
-}
+const lcm = (a, b) => {
+  return (a * b) / gcd(a, b);
+};
