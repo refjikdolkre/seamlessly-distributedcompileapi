d1a44eb324adb43a4d291ccc55290912edc0969e
refjikdolkre <msjcrs.engineer@hotmail.com>
2024-05-15 01:19:52 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = triangle[n - 1];
-  for (let i = n - 2; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
-    }
-  }
-  return dp[0];
+function countVowels(str) {
+  return str.match(/[aeiou]/gi).length;
 }
