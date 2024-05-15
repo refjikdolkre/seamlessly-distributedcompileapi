757e31f73df35b712674ff303d2a279782cd723e
refjikdolkre <msjcrs.engineer@hotmail.com>
2024-05-15 18:59:46 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function countVowels(str) {
-  return str.match(/[aeiou]/gi).length;
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(permutation) {
+    if (permutation.length === nums.length) {
+      result.push([...permutation]);
+      return;
+    }
+    for (const num of nums) {
+      if (!permutation.includes(num)) {
+        permutation.push(num);
+        backtrack(permutation);
+        permutation.pop();
+      }
+    }
+  }
 }
